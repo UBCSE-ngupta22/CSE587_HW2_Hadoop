71c42df916b80c62fc1505621b65f3fed829c9e2
Nikhil Gupta <nguptaa183@gmail.com>
2023-11-07 19:28:11 -0500

    Add WordCount

--- WordCount.java
+++ WordCount.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.net.URI;
 import java.util.Map;
 import java.util.*;
 
@@ -10,9 +12,12 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.StringUtils;
 
 public class WordCount {
 
@@ -22,30 +27,85 @@ public class WordCount {
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
 
+    private Set<String> patternsToSkip = new HashSet<String>();
 
+    private Configuration conf;
+    private BufferedReader fis;
+
+    @Override
+    public void setup(Context context) throws IOException,
+        InterruptedException {
+      conf = context.getConfiguration();
+      if (conf.getBoolean("wordcount.skip.patterns", false)) {
+        URI[] patternsURIs = Job.getInstance(conf).getCacheFiles();
+        for (URI patternsURI : patternsURIs) {
+          Path patternsPath = new Path(patternsURI.getPath());
+          String patternsFileName = patternsPath.getName().toString();
+          parseSkipFile(patternsFileName);
+        }
+      }
+    }
+
+    private void parseSkipFile(String fileName) {
+      try {
+        fis = new BufferedReader(new FileReader(fileName));
+        String pattern = null;
+        while ((pattern = fis.readLine()) != null) {
+          patternsToSkip.add(pattern);
+        }
+      } catch (IOException ioe) {
+        System.err.println("Caught exception while parsing the cached file '"
+            + StringUtils.stringifyException(ioe));
+      }
+    }
+
+    @Override
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 
-      StringTokenizer itr = new StringTokenizer(value.toString());
+
+      String line = value.toString().toLowerCase();
+      line = line.replaceAll("\\p{P}","");
+      for (String pattern : patternsToSkip) {
+        line = line.replaceAll("\\b" + pattern + "\\b", "");
+      }
+
+      StringTokenizer itr = new StringTokenizer(line);
       while (itr.hasMoreTokens()) {
-        String strTrimmed = itr.nextToken().replaceAll("\\p{P}","").toLowerCase();
-        word.set(strTrimmed);
+        word.set(itr.nextToken());
         context.write(word, one);
       }
     }
   }
 
-  public static class IntSumReducer
-      extends Reducer<Text, IntWritable, Text, IntWritable> {
-        
-    private HashMap<Text, Integer> resultMap = new HashMap<>();
+  public static class IntSumCombiner
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
 
     public void reduce(Text key, Iterable<IntWritable> values,
-        Context context) throws IOException, InterruptedException {
-      
+                       Context context
+                       ) throws IOException, InterruptedException {
       int sum = 0;
       for (IntWritable val : values) {
         sum += val.get();
       }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+
+    public static class IntSumReducer
+      extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    private HashMap<Text, Integer> resultMap = new HashMap<>();
+
+    public void reduce(Text key, Iterable<IntWritable> values,
+        Context context) throws IOException, InterruptedException {
+
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+
       resultMap.put(new Text(key), sum);
 
     }
@@ -65,19 +125,36 @@ public class WordCount {
           }
       }
     }
-
+  
   }
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
+    GenericOptionsParser optionParser = new GenericOptionsParser(conf, args);
+    String[] remainingArgs = optionParser.getRemainingArgs();
+    if ((remainingArgs.length != 2) && (remainingArgs.length != 4)) {
+      System.err.println("Usage: wordcount <in> <out> [-skip skipPatternFile]");
+      System.exit(2);
+    }
     Job job = Job.getInstance(conf, "word count");
     job.setJarByClass(WordCount.class);
     job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumCombiner.class);
     job.setReducerClass(IntSumReducer.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
-    FileInputFormat.addInputPath(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    List<String> otherArgs = new ArrayList<String>();
+    for (int i=0; i < remainingArgs.length; ++i) {
+      if ("-skip".equals(remainingArgs[i])) {
+        job.addCacheFile(new Path(remainingArgs[++i]).toUri());
+        job.getConfiguration().setBoolean("wordcount.skip.patterns", true);
+      } else {
+        otherArgs.add(remainingArgs[i]);
+      }
+    }
+    FileInputFormat.addInputPath(job, new Path(otherArgs.get(0)));
+    FileOutputFormat.setOutputPath(job, new Path(otherArgs.get(1)));
+
     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 }
